a0c0cb19c3d358dc6a9d1ae3d0d549ef93a98e93
Newton <newton1804090@gmail.com>
2025-06-25 16:04:52 +0600

    feat: created sign up and login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotebookLMUI from './modules/notebookLMUI/NotebookLMUI';
 import NotebookLMInterface from './modules/notebookLMInterface/NotebookLMInterface';
+import AuthUI from './modules/login/AuthUI';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<NotebookLMUI />} />
       <Route path="/notebook" element={<NotebookLMInterface />} />
+      <Route path="/login" element = {< AuthUI />} />
     </Routes>
   </Router>
 );
